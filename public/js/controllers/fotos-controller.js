aed2cda169c492b982d07c878d6be31ee8013e3d
Julio Cesar Cabral <julio.cabral@b2wdigital.com>
2016-11-24 12:22:42 -0200

    first commit

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,12 @@
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
+  $scope.fotos = [];
+  var promise = $http.get('/v1/fotos'); 
+
+  promise.then(function(retorno) {
+        $scope.fotos = retorno.data;
+  })
+  .catch(function(erro) {
+        console.log(erro)
+    }); 
+
+});
