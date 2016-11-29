a4a8c6b64cd88becfe66b54c9d01c41874822665
Julio Cesar Cabral <julio.cabral@b2wdigital.com>
2016-11-29 11:56:19 -0200

    criando diretiva

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -2,11 +2,13 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
   $scope.fotos = [];
   var promise = $http.get('/v1/fotos'); 
 
-  promise.then(function(retorno) {
-        $scope.fotos = retorno.data;
-  })
-  .catch(function(erro) {
-        console.log(erro)
-    }); 
+  $http.get('/v1/fotos')
+    .success(function(retorno) {
+        console.log(retorno);
+        $scope.fotos = retorno; // não precisa fazer retorno.data
+    })
+    .error(function(erro) {
+        console.log(erro);
+    });
 
 });
