8775fb3bf86b8f182ff9b1d7e44087e3cb6d1433
Julio Cesar Cabral <julio.cabral@b2wdigital.com>
2016-12-14 11:42:55 -0200

    exclusao e edicao

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -12,4 +12,19 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
         console.log(erro);
     });
 
+  $scope.remover = function(foto) {
+
+        $http.delete('/v1/fotos/' + foto._id)
+        .success(function() {
+            var indiceDaFoto = $scope.fotos.indexOf(foto);
+            $scope.fotos.splice(indiceDaFoto, 1);
+            $scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso!';
+
+        })
+        .error(function(erro) {
+            console.log(erro);
+            $scope.mensagem = 'Não foi possível apagar a foto ' + foto.titulo;
+        });
+    };
+
 });
